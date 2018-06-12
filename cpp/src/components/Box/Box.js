8d1de55cea81bd96986a34fca2a34bf07fd99891
lucasromerodb <lucasromerodb@gmail.com>
2018-06-11 21:48:27 -0300

    create first Box component

---
+++ cpp/src/components/Box/Box.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export class Box extends Component {
+  constructor() {
+    super()
+    this.state = { color: 'white' }
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
